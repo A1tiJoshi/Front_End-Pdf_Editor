bfd26b0257403eac35ad1fab388d10f1e462ab51
Avanti Joshi <a1tijoshi@gmail.com>
2024-07-29 22:08:00 +0530

    commit

---
+++ config.js
@@ -0,0 +1,8 @@
+window.suprsendConfig = {
+    env: 'production', // 'production' or 'development'
+    user_id: 'user123', // static user ID for testing or dynamically set this based on your application logic
+    workspace_key: 'bBtAUGMGPi2lhsmnlYen', // your actual workspace key
+    workspace_secret: 'SS.WSS.7rUtOXW9UCSg13qpwCeIvgipoh1qUqTlPFeJITuE' // your actual workspace secret
+};
+
+
